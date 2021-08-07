781180524ce917d36401194d0c05586a85cf4dd5
Tungsinn <gvnalamas@tutanota.com>
2021-08-07 18:40:27 -0300

    Consumo de API

--- src/Pages/MainPage/index.js
+++ src/Pages/MainPage/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../Services/api';
 import {HeaderContainer} from '../../Components/Header'
 import {ContainerPage, ContainerMain, ContainerProdutos, ContainerButtons} from './style'
 import Produto from '../../Components/Produto'
@@ -6,6 +7,60 @@ import Footer from '../../Components/Footer'
 import {Link} from 'react-router-dom';
 
 function MainPage() {
+  const [ products, setProducts ] = useState([]);
+  const [ page, setPage ] =  useState(1);
+  const limit = 6;
+  const [ hasMoreProducts, setHasMoreProducts ] = useState(true);
+  const [activeFilter, setActiveFilter] = useState('');
+  
+  async function getProducts(page, limit, category) {
+    const response = await api.get('products/', {
+      params: {
+        page: page,
+        limit: limit,
+        category: category
+      }
+    });
+    console.log(response);
+
+    try {
+      if(response.status === 200) {
+        if(page === 1)
+          setProducts(response.data);
+        else setProducts(products.concat(response.data));
+      }
+
+      if(response.data.length < limit)
+        setHasMoreProducts(false);
+      else setHasMoreProducts(true);
+
+    } catch(error) {
+      console.log(error);
+      setHasMoreProducts(false);
+    }
+  }    
+
+  function filterByCategory(category) {
+    setPage(1);
+
+    if(category === activeFilter) {
+      setActiveFilter('');
+      getProducts(1, limit);
+    } else {
+      setActiveFilter(category);
+      getProducts(1, limit, category);
+    }
+  }
+
+  function loadMoreProducts() {
+    const newPage = page + 1;
+    getProducts(newPage, limit);
+    setPage(newPage);
+  }
+
+  useEffect(() => {
+    getProducts(page, limit);
+  }, []);
 
   return (
     <ContainerPage>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 import Login from './Pages/LoginPage';
+import Cadastro from './Pages/CadastroPage';
 import MainPage from './Pages/MainPage';
 import ConfirmacaoPage from './Pages/ConfirmacaoPage';
 import CarrinhoPage from './Pages/CarrinhoPage'
@@ -10,6 +11,7 @@ function Routes() {
   return (
     <BrowserRouter>
         <Route path='/Login' component={Login} />
+        <Route path='/Cadastro' component={Cadastro} />
         <Route exact path='/' component={MainPage} />
         <Route path='/Confirmacao' component={ConfirmacaoPage} />
         <Route path='/Carrinho' component={CarrinhoPage} />
